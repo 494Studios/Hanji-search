78c841c7da5183d49eeec7ca1b0f6c6d71f6728d
Akash Eldo <akasheldo@hotmail.com>
2018-12-27 19:04:20 -0500

    Indexer not clears index before adding documents

--- src/main/java/com/hanji/search/IndexerEngine.java
+++ src/main/java/com/hanji/search/IndexerEngine.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -24,14 +25,41 @@ public class IndexerEngine extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("UTF-8");
 
+        response.getWriter().println("Deleted: " + clearIndex() + " documents from index");
         Database db = new Database();
         try {
             Map<String,Object> data = db.fetchDoc("가하다1");
-            response.getWriter().println(data);
-            //addDocument(data);
+            response.getWriter().println("Added: " + data);
+            addDocument(data);
         }catch (InterruptedException | ExecutionException e){
             e.printStackTrace();
         }
+    }
+
+    private int clearIndex(){
+        int count = 0;
+        try {
+            // looping because getRange by default returns up to 100 documents at a time
+            while (true) {
+                List docIds = new ArrayList<>();
+                // Return a set of doc_ids.
+                GetRequest request = GetRequest.newBuilder().setReturningIdsOnly(true).build();
+                GetResponse response = getIndex().getRange(request);
+                if (response.getResults().isEmpty()) {
+                    break;
+                }
+                for (Object doc : response) {
+                    docIds.add(((Document)doc).getId());
+                    count++;
+                }
+                getIndex().delete(docIds);
+            }
+            // Delete the index schema
+            getIndex().deleteSchema();
+        } catch (RuntimeException e) {
+            System.out.println("Failed to delete index");
+        }
+        return count;
     }
 
     private void addDocument(Map<String, Object> data) {
@@ -76,5 +104,10 @@ public class IndexerEngine extends HttpServlet {
             }
             break;
         }
+    }
+
+    private Index getIndex() {
+        IndexSpec indexSpec = IndexSpec.newBuilder().setName(IndexerEngine.INDEX_NAME).build();
+        return SearchServiceFactory.getSearchService().getIndex(indexSpec);
     }
 }
