3a0c33f373ba09e3830ff86dfbbc5754a7e5289b
Akash Eldo <akasheldo@hotmail.com>
2018-12-28 10:01:00 -0500

    Created a ContextListener to add all documents to index, no longer handled by /index IndexerEngine now only clears the index Made db object a Singleton in Database Created getAllDocs() method in Database for indexing MyContextListener now handles indexing documents. Should be done on warmup

--- src/main/java/com/hanji/search/Database.java
+++ src/main/java/com/hanji/search/Database.java
@@ -5,13 +5,16 @@ import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.auth.oauth2.GoogleCredentials;
 
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -20,41 +23,54 @@ import java.util.concurrent.ExecutionException;
  */
 public class Database {
 
-    private Firestore db;
+    private static Firestore db = null;
 
-    public Database(){
-        // Use a service account
-        try {
-            InputStream serviceAccount = new FileInputStream("C:\\Users\\akash\\Projects\\Hanji Search\\hanjisearch\\hanji-bd63d-849ae0babd80.json");
-            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(credentials)
-                    .build();
-            if(FirebaseApp.getApps().isEmpty()) { //<--- check with this line
+    public Database() {
+        if (db == null) {
+            try {
+                // Use the application default credentials
+                GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(credentials)
+                        .setProjectId("hanji-bd63d")
+                        .build();
                 FirebaseApp.initializeApp(options);
+                if (FirebaseApp.getApps().isEmpty()) { //<--- check with this line
+                    FirebaseApp.initializeApp(options);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }catch (IOException e){
+            db = FirestoreClient.getFirestore();
+        }
+    }
+
+    public ArrayList<Map<String,Object>> getAllDocs(){
+        ArrayList<Map<String,Object>> docs = new ArrayList<>();
+
+        //asynchronously retrieve all documents
+        ApiFuture<QuerySnapshot> future = db.collection("words").get();
+        try {
+            // future.get() blocks on response
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                System.out.println(new String(document.getId().getBytes(), Charset.forName("UTF-8")));
+
+                Map<String,Object> map = document.getData();
+                map.put("id",document.getId());
+                docs.add(map);
+            }
+        }catch (ExecutionException | InterruptedException e){
             e.printStackTrace();
         }
-        db = FirestoreClient.getFirestore();
+        return docs;
     }
 
     public Map<String,Object> fetchDoc(String id) throws InterruptedException,ExecutionException {
-        ApiFuture<DocumentSnapshot> query = this.db.collection("words").document(id).get();
+        ApiFuture<DocumentSnapshot> query = Database.db.collection("words").document(id).get();
         DocumentSnapshot documentSnapshot = query.get();
         Map<String,Object> map = documentSnapshot.getData();
         map.put("id",documentSnapshot.getId());
         return map;
-    }
-
-    public static void main(String[] args){
-        Database database = new Database();
-
-        // asynchronously retrieve all users
-        try {
-            System.out.println(database.fetchDoc("가하다1"));
-        }catch (InterruptedException | ExecutionException e){
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/com/hanji/search/IndexerEngine.java
+++ src/main/java/com/hanji/search/IndexerEngine.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @author Akash Eldo (axe1412)
@@ -26,14 +24,6 @@ public class IndexerEngine extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
 
         response.getWriter().println("Deleted: " + clearIndex() + " documents from index");
-        Database db = new Database();
-        try {
-            Map<String,Object> data = db.fetchDoc("가하다1");
-            response.getWriter().println("Added: " + data);
-            addDocument(data);
-        }catch (InterruptedException | ExecutionException e){
-            e.printStackTrace();
-        }
     }
 
     private int clearIndex(){
@@ -60,50 +50,6 @@ public class IndexerEngine extends HttpServlet {
             System.out.println("Failed to delete index");
         }
         return count;
-    }
-
-    private void addDocument(Map<String, Object> data) {
-        Document.Builder builder = Document.newBuilder()
-                .addField(Field.newBuilder().setName("id").setText(data.get("id").toString()))
-                .addField(Field.newBuilder().setName("term").setText(data.get("term").toString()));
-        ArrayList<String> definitions = (ArrayList<String>) data.get("definitions");
-        for (String def : definitions) {
-            builder.addField(Field.newBuilder().setName("definition").setText(def));
-        }
-        Document doc = builder.build();
-
-        try {
-            indexADocument(INDEX_NAME, doc);
-        } catch (InterruptedException e) {
-            System.err.println("Couldn't add document to index");
-            e.printStackTrace();
-        }
-    }
-
-    // From https://cloud.google.com/appengine/docs/standard/java/search/
-    private static void indexADocument(String indexName, Document document)
-            throws InterruptedException {
-        IndexSpec indexSpec = IndexSpec.newBuilder().setName(indexName).build();
-        Index index = SearchServiceFactory.getSearchService().getIndex(indexSpec);
-
-        final int maxRetry = 3;
-        int attempts = 0;
-        int delay = 2;
-        while (true) {
-            try {
-                index.put(document);
-            } catch (PutException e) {
-                if (StatusCode.TRANSIENT_ERROR.equals(e.getOperationResult().getCode())
-                        && ++attempts < maxRetry) { // retrying
-                    Thread.sleep(delay * 1000);
-                    delay *= 2; // easy exponential backoff
-                    continue;
-                } else {
-                    throw e; // otherwise throw
-                }
-            }
-            break;
-        }
     }
 
     private Index getIndex() {
